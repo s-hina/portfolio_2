a1bbdc91ab01419c4c7776203a071319a1e7e222
mi-ogura <m-ogura@cmind-co.jp>
2020-08-10 01:22:07 +0900

    わ

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,27 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\HTML;
+
+use App\News;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+   public function index(Request $request)
     {
-        $this->middleware('auth');
-        
-    }
+        $cond_title = $request->cond_title;
+        // $cond_title が空白でない場合は、記事を検索して取得する
+        if ($cond_title != '') {
+            $posts = News::where('title', $cond_title).orderBy('updated_at', 'desc')->get();
+        } else {
+            $posts = News::all()->sortByDesc('updated_at');
+        }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
+        if (count($posts) > 0) {
+            $headline = $posts->shift();
+        } else {
+            $headline = null;
+        }
+
+        // news/index.blade.php ファイルを渡している
+        // また View テンプレートに headline、 posts、 cond_title という変数を渡している
+        return view('news.index', ['headline' => $headline, 'posts' => $posts, 'cond_title' => $cond_title]);
     }
 }
--- database/migrations/2020_07_29_045337_create_post_table.php
+++ database/migrations/2020_07_29_045337_create_post_table.php
@@ -17,7 +17,7 @@ class CreatePostTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')
-            ->references('id')->on('user')
+            ->references('id')->on('users')
             ->onDelete('cascade');
             $table->string('title');
             $table->string('photo');
